7eed7232a21dd47c8d4e98b998537891b574fe53
Boubakri Nawres <47699352+nawresboubakri@users.noreply.github.com>
2020-03-02 13:29:18 +0100

    Add files via upload

---
+++ nawres/integration finale/choix.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+menu_choix()
+{
+	echo
+	echo
+	echo
+	echo "			******************Choisir un menu : ******************"
+	echo
+	echo
+			echo "1) -NCURSUS"
+			echo "2) -YAD "
+			read choix
+}
